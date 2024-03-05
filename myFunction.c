a27defa99abd114feb1164acca12ee484fd395b1
MrShuster <alon.s@e.email>
2024-03-05 20:52:56 +0200

    make splitArgument work without strtok

--- myFunction.c
+++ myFunction.c
@@ -19,7 +19,17 @@ char *getInputFromUser()
     return str;
 }
 // עליכם לממש את הפונקציה strtok כלומר שהפונקציה הנ"ל תבצע בדיוק אותו הדבר רק בלי השימוש בפונקציה strtok
-char **splitArgument(char *str)
+char **splitArgument(char *str) {
+    int size = 1; // Start with size 1 to account for NULL at the end
+    int index = 0;
+    int start = 0; // Starting index of a word
+    char **arguments = (char **)malloc(size * sizeof(char *));
+    int length = strlen(str);
+    
+    for (int i = 0; i <= length; i++) {
+        // Check for end of a word or end of the string
+        if (str[i] == ' ' || str[i] == '\0') {
+            // Calculate word lengthchar **splitArgument(char *str)
 {
     // str = cp file file file
     //[cp,file,file,file,NULL]
@@ -40,6 +50,25 @@ char **splitArgument(char *str)
 
     return argumnts;
 }
+            int wordLength = i - start;
+            if (wordLength > 0) {
+                // Allocate memory for the new word
+                arguments[index] = (char *)malloc((wordLength + 1) * sizeof(char));
+                memcpy(arguments[index], &str[start], wordLength);
+                arguments[index][wordLength] = '\0'; // Null-terminate the word
+                index++;
+                size++;
+                // Reallocate memory for the arguments array to accommodate the new word
+                arguments = (char **)realloc(arguments, size * sizeof(char *));
+            }
+            start = i + 1; // Move start to the next word
+        }
+    }
+    arguments[index] = NULL; // Null-terminate the array of words
+
+    return arguments;
+}
+
 // בפונקציה הנ"ל קיבלנו את הנתיב ממנו אנחנו מריצים את התוכנית שלנו
 //  עליכם לשדרג את הנראות של הנתיב כך ש-בתחילת הנתיב יופיע שם המחשב (כמו בטרמינל המקורי) בסוף יופיע הסימן דולר
 //  ולאחר הדולר ניתן אפשרות למשתמש להזין מחרוזת מבלי שנרד שורה.
